9264f2740dcbc34ec56c1981867e1f9d65ac85cb
lazzieecoderr <chakravarthyrrc@gmail.com>
2024-03-26 13:02:51 +0530

    updated

---
+++ script.js
@@ -0,0 +1,15 @@
+/*Printing JSON using for of and for each
+const detail = {
+    "name":"test",
+    "age":"15"
+}
+
+const data = Object.entries(detail)
+console.log(data);
+for(var [key,value] of data){
+    console.log(value);
+}
+data.forEach(([key,val])=>console.log(val));
+*/
+
+
